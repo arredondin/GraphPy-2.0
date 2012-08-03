5eebe46450a6fabb4da32ebbb22d71a71b1f6384
rcisterna <r.cisternasantos@gmail.com>
2012-08-02 22:58:37 -0400

    Implementado "main"

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2
+
+def main():
+	print 'Iniciando GraphPy utilizando el interprete local de python'
+	print '...'
+	import controller
+	print 'GraphPy se ha ejecutado correctamente'
+
+if __name__ == "__main__":
+    main()
